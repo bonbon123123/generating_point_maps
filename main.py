22abf03f8333b355ce3ea06b828bc839afa4714e
bonbon123123 <132760225+PiotrStach@users.noreply.github.com>
2024-05-13 14:28:54 +0200

    added land based system to manage diffrent structures, mising collor/tag system so the same type of structure will merge with oneanother. tried and failed to make hulls of land merge as intended wip

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ def orientation(p, q, r):
     return 1 if val > 0 else -1
 
 
-def jarvis_marszuje(points,mapEditor):
+def jarvis_marszuje(points,mapEditor,land_to_grow=None):
     n = len(points)
     if n < 3:
         return points
@@ -67,13 +67,124 @@ def jarvis_marszuje(points,mapEditor):
         if p == leftmost_index:
             break
 
-    for point in hull:
-        point.color="green"
-        mapEditor.add_point(point)
+    if land_to_grow==None:
+        land = Land(hull)
+        mapEditor.add_land(land)
+    else:
+        print("mamy land do rosnięcia")
+        land_to_grow.grow_land(hull)
+    # for point in hull:
+    #     point.color="green"
+    #     mapEditor.add_point(point)
+    #
+    # for i in range(len(hull)):
+    #     mapEditor.add_line(hull[i], hull[(i + 1) % len(hull)])
 
-    for i in range(len(hull)):
-        mapEditor.add_line(hull[i], hull[(i + 1) % len(hull)])
+class Land:
+    def __init__(self, points):
+        self.points = points
+        self.lines = []
+        for i in range(len(points)):
+            self.lines.append(Line(points[i], points[(i + 1) % len(points)]))
 
+    def add_line(self, line):
+        self.lines.append(line)
+
+    def add_point(self, point):
+        self.points.append(point)
+
+class Land:
+    def __init__(self, points):
+        self.points = points
+        self.lines = []
+        for i in range(len(points)):
+            self.lines.append(Line(points[i], points[(i + 1) % len(points)]))
+
+    def add_line(self, line):
+        self.lines.append(line)
+
+    def add_point(self, point):
+        self.points.append(point)
+
+    def grow_land(self, hull):
+        common_points = [p for p in hull if p in self.points]
+        for point in hull:
+            for point1 in self.points:
+                if(point.x==point1.x and point.y==point1.y ):
+                    print(point.x,point.y)
+        print("nie moje punkty:")
+        for point1 in hull:
+            print(point1.x,point1.y)
+        print("moje punkty:")
+        for point1 in self.points:
+            print(point1.x,point1.y)
+        if len(common_points) == 1 or len(common_points) == 2:
+
+            for point in hull:
+                print("point.x:")
+                print(point.x)
+
+                if point not in self.points:
+                    index = self.points.index(common_points[-1]) + 1
+                    self.points.insert(index, point)
+        else:
+            first_common = self.points.index(common_points[0])
+            last_common = self.points.index(common_points[-1])
+
+            new_points = []
+            for point in hull:
+                print("point.x:")
+                print(point.x)
+                if point not in self.points:
+                    new_points.append(point)
+
+            self.points = self.points[:first_common + 1] + new_points + self.points[last_common:]
+
+        self.lines = []
+        for i in range(len(self.points)):
+            self.lines.append(Line(self.points[i], self.points[(i + 1) % len(self.points)]))
+
+
+
+def update_land(self, map_editor):
+        for line in self.lines:
+            map_editor.add_line(line.p1, line.p2)
+
+        for point in self.points:
+            point.color = "green"
+            map_editor.add_point(point)
+
+
+# def convex_hull(points,mapEditor):
+#     n = len(points)
+#     if n < 3:
+#         return "Za mało punktów"
+#
+#
+#     points.sort(key=lambda point: point.x)
+#
+#     lower_hull = []
+#     for point in points:
+#         while len(lower_hull) >= 2 and orientation(lower_hull[-2], lower_hull[-1], point) != 2:
+#             lower_hull.pop()
+#         lower_hull.append(point)
+#
+#     upper_hull = []
+#     for point in reversed(points):
+#         while len(upper_hull) >= 2 and orientation(upper_hull[-2], upper_hull[-1], point) != 2:
+#             upper_hull.pop()
+#         upper_hull.append(point)
+#
+#     upper_hull.pop()
+#
+#     hull = lower_hull + upper_hull
+#
+#     for point in hull:
+#         point.color="green"
+#         mapEditor.add_point(point)
+#
+#     for i in range(len(hull)):
+#         mapEditor.add_line(hull[i], hull[(i + 1) % len(hull)])
 
 
 class MapEditor:
@@ -81,6 +192,7 @@ class MapEditor:
         self.fig, self.ax = plt.subplots(figsize=(10, 10))
         self.points = []
         self.lines = []
+        self.lands=[]
         self.plot_size = 1000
         self.search_range=100
         self.range_of_adding=1
@@ -101,6 +213,8 @@ class MapEditor:
 
     def add_point(self, p):
         self.points.append(p)
+    def add_land(self,land):
+        self.lands.append(land)
     def create_map_area(self, x, y):
         area_size = self.plot_size / self.rows
         adjacent_areas = []
@@ -138,17 +252,25 @@ class MapEditor:
 
             new_point = Point(clicked_x, clicked_y, "blue")
             points_to_do=[]
-            has_green_point = False
+
             for point in self.points:
                 distance = new_point.distance_from_other_point(point)
                 if distance <= self.search_range:
                     points_to_do.append(point)
-                    if point.color == "green":
-                        has_green_point = True
 
-            if has_green_point:
-                green_points = [point for point in self.points if point.color == "green"]
-                points_to_do.extend(green_points)
+            has_land_point = False
+            for land in self.lands:
+                for point in land.points:
+                    distance = new_point.distance_from_other_point(point)
+                    if distance <= self.search_range:
+                        print("znalezion")
+                        jarvis_marszuje(points_to_do,self,land)
+                        has_land_point = True
+                        break
+
+            if not has_land_point:
+                self.lines=[]
+                jarvis_marszuje(points_to_do,self)
 
             for point in points_to_do:
                 if point in self.points:
@@ -169,9 +291,15 @@ class MapEditor:
         else:
             self.ax.set_xlim(0, 1000)
             self.ax.set_ylim(0, 1000)
-        for line in self.lines:
-            plt.plot([line.p1.x, line.p2.x], [line.p1.y, line.p2.y], 'r-', marker='o')
+        for land in self.lands:
+            for line in land.lines:
+                plt.plot([line.p1.x, line.p2.x], [line.p1.y, line.p2.y], 'r-', marker='o')
 
+            for point in land.points:
+                self.ax.plot(point.x, point.y, 'o', color=point.color)
+        # for line in self.lines:
+        #     plt.plot([line.p1.x, line.p2.x], [line.p1.y, line.p2.y], 'r-', marker='o')
+        #
         for point in self.points:
             self.ax.plot(point.x, point.y, 'o', color=point.color)
 
